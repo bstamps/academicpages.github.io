6004aaa5ccfb337694d02e3789419f354c8b91ce
Blake Stamps <bstamps@mines.edu>
2018-04-20 09:20:44 -0600

    Talkmap Fix
    Seeing if this fixes my map.

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,17 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "Golden, Colorado",
+    39.755543,
+    -105.2210997
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
+    "Norman, Oklahoma",
+    35.2225717,
+    -97.4394816
   ],
   [
     "San Francisco, California",
-    37.7792808,
-    -122.4192362
-  ],
-  [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    8.6545394,
+    -73.3676149
   ]
 ];
